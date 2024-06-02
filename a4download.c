39689b29fd9c6872f84b05c5ff121bb3640bd292
Alec Odell <alec_odell@outlook.com>
2024-06-01 17:04:42 -0700

    gets first line from download file

--- a4download.c
+++ a4download.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[])
             printf("Error opening file '%s'\n", argv[1]);
             return EXIT_FAILURE;
         }
-        char *cur_num = argv[2];
-        while (*cur_num != 0)
+        char *cnum = argv[2];
+        while (*cnum != 0)
         {
-            if (*cur_num > 47 && *cur_num < 58)
+            if (*cnum > 47 && *cnum < 58)
             {
-                max = (max * 10) + (*cur_num - 48);
-                cur_num++;
+                max = (max * 10) + (*cnum - 48);
+                cnum++;
             }
             else
             {
@@ -33,7 +33,30 @@ int main(int argc, char *argv[])
         }
     }
 
-    printf("Max: %d \n", max);
+    char cline[512];
+    char *oname = fgets(cline, sizeof(cline), file);
+    char *url = oname;
+    int timeout;
+
+    while (*url != 10 && *url != 32)
+    {
+        ++url;
+    }
+    *url++ = 0;
+    char *cchar = url;
+    while (*cchar != 10 && *cchar != 32)
+    {
+        ++cchar;
+    }
+    *cchar++ = 0;
+    while (*cchar != 10 && *cchar != 32)
+    {
+        timeout = (timeout * 10) + (*cchar - 48);
+        ++cchar;
+    }
+    *cchar++ = 0;
+
+    printf("Name: %s , URL: %s , Timeout: %d , Max: %d\n", oname, url, timeout, max);
 
     return (0);
 }
